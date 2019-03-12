47cefcb91fe71ae50bbc21995971501224344abc
Konstantin Polyakov <shyzik93@mail.ru>
2019-03-12 14:13:05 +0300

    self was added

--- lib.js
+++ lib.js
@@ -6,16 +6,17 @@
 /* y_examples - for studing only */
 function Sets_Array(x_examples, y_examples) {
 
+    let self = this;
+
     this._x_examples = x_examples;
     this._y_examples = y_examples;
-
     this.length = this._x_examples.length;
 
     this.get_x_example = function(ix) {
-        return this._x_examples[ix].slice(0);
+        return self._x_examples[ix].slice(0);
     }
 
     this.get_y_example = function(iy) {
-        if (this._y_examples !== undefined) return this._y_examples[iy];
+        if (self._y_examples !== undefined) return self._y_examples[iy];
     }
 }
