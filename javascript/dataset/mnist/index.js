8002c2beed3a3b126d8b289984892978009865d1
Konstantin Polyakov <shyzik93@mail.ru>
2019-03-19 16:04:54 +0300

    Main class was improved for big MNIST-data array.

--- javascript/dataset/mnist/index.js
+++ javascript/dataset/mnist/index.js
@@ -1,9 +1,14 @@
 function DataSeter() {
 
     self = this;
-    this.NL_dataset = null;
+    this.sub_dataset = null;
+    this.pair = null;
+    this.f = null;
+    
 
-    this.length = 4;
+    this.length = 100;
+
+    // входов: 784
 
     /*this.import_datasets2 = function(name) {
         let script = document.getElementById('NL_dataset_el');
@@ -21,16 +26,29 @@ function DataSeter() {
         var req = new XMLHttpRequest();
         req.open('get', url, false);
         req.send();
-        self.NL_dataset = JSON.parse(req.responseText);
+        self.sub_dataset = JSON.parse(req.responseText);
+    }
+
+    this.get_dataset = function(index) {
+        let i = index % 50; // индекс в поднаборе
+        let f = (index  -  i) + 50; // имя файла
+        let fname = 'mnist/dataset/'+f;
+
+        if (self.sub_dataset === null | self.f !== f) {
+            self.f = f;
+            self.import_dataset(fname);
+        }
+        self.pair = self.sub_dataset[i];
     }
 
     this.get_x_example = function(ix) {
-        self.import_dataset('mnist/dataset/'+ix);
-        return self.NL_dataset[0][0];
+        self.get_dataset(ix);
+        return self.pair[1];
     }
+
     this.get_y_example = function(iy) {
-        self.import_dataset('mnist/dataset/'+iy);
-        return [self.NL_dataset[0][1]];
+        self.get_dataset(iy);
+        return self.pair[0];
     } 
 
 }
